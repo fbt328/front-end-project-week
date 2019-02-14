3c444ec0c0d7300a67991ae11932241bec4d6733
fbt328 <katherine.rust.andrews@gmail.com>
2019-02-13 19:36:11 -0800

    note submitter form added

---
+++ lambdanotes/src/components/NoteSubmitter.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+class NoteSubmitter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      textBody: '',
+      tag: ''
+    };
+  }
+ 
+  addNote = e => {
+    e.preventDefault();
+    Axios.post('https://fe-notes.herokuapp.com/note/create', {
+        title: this.state.title, 
+        textBody: this.state.textBody })
+    .then (() => {
+      this.setState({
+        title: '',
+        textBody: '',
+        tag: ''})
+    })
+    .catch (err => console.log(err))
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="NoteFormContainer">
+      <h1>Create New Note</h1>
+        <form onSubmit={this.addNote}>
+          <input
+            onChange={this.handleInputChange}
+            placeholder="Note Title"
+            value={this.state.title}
+            name="title"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="Note Content"
+            value={this.state.textBody}
+            name="textBody"
+          />
+          
+          <button type="saveNoteButton">Save</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NoteSubmitter;
